1abcac05074e19afa15e7f80ed61256f2c1bacf1
marawan salman <marawansalman98@gmail.com>
2020-11-11 18:00:05 +0200

    last update

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/the-pyramids', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect('mongodb+srv://marawansalman:maromaro1212@cairo-belguim.xqxi1.mongodb.net/cairo-belguim', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Your DB (the-pyramids) Connected");
 });
+
